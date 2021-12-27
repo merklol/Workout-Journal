40b6bf481efdf18a1fa17d4b049237d3724e7458
maximsmolyakov <sm.maksim@icloud.com>
2021-12-27 22:53:38 +0800

    Create app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+import extensions.configuration
+import extensions.implementation
+
+plugins {
+    `android-app-convention`
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+configuration(
+    applicationId = "com.maximapps.workoutjournal",
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+)
+
+dependencies {
+    implementation(Libraries.room())
+    implementation(Libraries.navigation())
+    implementation(Libraries.daggerHilt())
+    implementation(Libraries.sharedPreferences)
+
+    implementation(project(":core"))
+    implementation(project(":core-ui"))
+    implementation(project(":features:main"))
+    implementation(project(":features:timer"))
+    implementation(project(":features:workout-note"))
+}
+
+kapt {
+    correctErrorTypes = true
+}
