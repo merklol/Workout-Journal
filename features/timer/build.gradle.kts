997e0e635f330034916a2ef77c4dece5d556ae37
maximsmolyakov <sm.maksim@icloud.com>
2021-12-28 11:51:38 +0800

    Add a 'timer' feature module

---
+++ features/timer/build.gradle.kts
@@ -0,0 +1,30 @@
+import extensions.configuration
+import extensions.implementation
+
+plugins {
+    `android-library-convention`
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+configuration(testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner")
+
+dependencies {
+    implementation(Libraries.navigation())
+    implementation(Libraries.daggerHilt())
+    implementation(Libraries.coroutines())
+    implementation(Libraries.lifecycle)
+    implementation(Libraries.viewBindingDelegate)
+
+    implementation(project(":core"))
+    implementation(project(":core-ui"))
+
+    testImplementation(Libraries.jUnit)
+    androidTestImplementation(Libraries.androidXJunit)
+    androidTestImplementation(Libraries.espresso)
+}
+
+kapt {
+    correctErrorTypes = true
+}
